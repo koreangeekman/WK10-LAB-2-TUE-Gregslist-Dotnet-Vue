b2557cfee65692f841d0f8acb75024a06bb6dda4
DJ Aaron <koreangeekman@gmail.com>
2023-11-28 09:13:30 -0700

    dotnet-vue template init

---
+++ server/Models/Account.cs
@@ -0,0 +1,9 @@
+namespace Gregslist-dotnet-vue.Models;
+
+public class Account
+{
+  public string Id { get; set; }
+  public string Name { get; set; }
+  public string Email { get; set; }
+  public string Picture { get; set; }
+}
